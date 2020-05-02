28e32390facb6c8638054339d76578c8ab070b04
edu <vizuete.eduardo@gmail.com>
2020-05-02 15:47:06 -0500

    module 3 lesson 3: Logging in a Spring Boot Project

--- task-management-app-lesson/src/main/java/com/baeldung/taskmanagementapplesson/service/impl/ProjectServiceImpl.java
+++ task-management-app-lesson/src/main/java/com/baeldung/taskmanagementapplesson/service/impl/ProjectServiceImpl.java
@@ -2,6 +2,8 @@ package com.baeldung.taskmanagementapplesson.service.impl;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.baeldung.taskmanagementapplesson.persistence.model.Project;
@@ -11,6 +13,8 @@ import com.baeldung.taskmanagementapplesson.service.IProjectService;
 @Service
 public class ProjectServiceImpl implements IProjectService {
 	
+	private static final Logger LOG = LoggerFactory.getLogger(ProjectServiceImpl.class);
+	
 	private IProjectRepository projectRepository;
 
 	public ProjectServiceImpl(IProjectRepository projectRepository) {
@@ -19,11 +23,13 @@ public class ProjectServiceImpl implements IProjectService {
 
 	@Override
 	public Optional<Project> findById(Long id) {
+		LOG.info("Project Service >> Finding Project By Id {}", id);
 		return projectRepository.findById(id);
 	}
 
 	@Override
 	public Project save(Project project) {
+		LOG.info("Project Service >> Saving Project", project);
 		return projectRepository.save(project);
 	}
 
