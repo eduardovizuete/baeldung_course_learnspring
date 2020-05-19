27b4c16bbfdcb0267e25bf4fc44c93c45a04dd95
edu <vizuete.eduardo@gmail.com>
2020-05-18 22:32:44 -0500

    module 6 lesson 1: Spring MVC

---
+++ task-management-app-lesson/src/main/java/com/baeldung/taskmanagementapplesson/web/controller/ProjectController.java
@@ -0,0 +1,20 @@
+package com.baeldung.taskmanagementapplesson.web.controller;
+
+import java.time.LocalDate;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.baeldung.taskmanagementapplesson.persistence.model.Project;
+
+@RestController
+@RequestMapping(value = "/projects")
+public class ProjectController {
+	
+	@GetMapping(path = "/1")
+	public Project findOne() {
+	    return new Project("testName", LocalDate.now());
+	}
+	
+}
