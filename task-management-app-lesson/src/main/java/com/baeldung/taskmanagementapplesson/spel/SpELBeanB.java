9b7c22df076a1b84ebd2e7c4a83dd3dfd63d6747
edu <gilberto.vizuete@modinter.com.ec>
2020-08-18 21:28:39 -0500

    Module 9 - lesson 3: The Spring Expression Language (SpEL)

---
+++ task-management-app-lesson/src/main/java/com/baeldung/taskmanagementapplesson/spel/SpELBeanB.java
@@ -0,0 +1,13 @@
+package com.baeldung.taskmanagementapplesson.spel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpELBeanB {
+
+    private Integer prop1 = 10;
+
+    public Integer getProp1() {
+        return prop1;
+    }
+}
